4fb24fe72bcd7fb6478dab693ef695a6b665439a
Andrés Díaz Soto <andres.diaz.soto@gmail.com>
2021-07-28 17:49:17 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: [process.env.API_DOMAIN],
+  },
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    config.module.rules.push({
+      test: /\.(graphql|gql)$/,
+      exclude: /node_modules/,
+      loader: 'graphql-tag/loader',
+    });
+    return config;
+  },
 }
