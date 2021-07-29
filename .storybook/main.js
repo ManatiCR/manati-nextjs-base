4e6e57746112eb2d9d94216cbcc9224e8e98de11
Andrés Díaz Soto <andres.diaz.soto@gmail.com>
2021-07-28 18:05:41 -0600

    Update storybook configuration

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,42 @@
+const path = require('path');
+const toPath = (_path) => path.join(process.cwd(), _path);
+
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  core: {
+    builder: 'webpack5',
+  },
+  stories: [
+    '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    'storybook-addon-next-router',
+  ],
+  webpackFinal: async (config) => {
+    const fileLoaderRule = config.module.rules.find(
+      (rule) => rule.test && rule.test.test(".svg")
+    );
+    fileLoaderRule.exclude = /\.svg$/;
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      enforce: "pre",
+      loader: require.resolve("@svgr/webpack")
+    });
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          '@emotion/core': toPath('node_modules/@emotion/react'),
+          '@emotion/styled': toPath('node_modules/@emotion/styled'),
+          'emotion-theming': toPath('node_modules/@emotion/react'),
+          '@babel/preset-react': toPath('node_modules/@babel/preset-react'),
+        },
+      },
+    };
+  },
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,39 @@
+import React from 'react';
+import { ThemeProvider } from 'theme-ui';
+import { Global, css } from '@emotion/react';
+import { RouterContext } from "next/dist/next-server/lib/router-context";
+import * as nextImage from 'next/image';
+import theme from '../src/theme';
+
+// Google Fonts. Change to match project design requirements.
+const globalStyles = css`
+  @import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;1,300;1,700&display=swap');
+`;
+
+// Next image mock component.
+Object.defineProperty(nextImage, 'default', {
+  configurable: true,
+  value: props => <div><img {...props} /></div>
+});
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+  nextRouter: {
+    Provider: RouterContext.Provider,
+  },
+};
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <Global styles={globalStyles} />
+      <Story />
+    </ThemeProvider>
+  ),
+];
